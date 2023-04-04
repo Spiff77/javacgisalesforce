77a2ee23cd3c6ec0205e9b15cee2d62ebe38bce6
Thomas Lhomme <lhomme.thomas@gmail.com>
2023-04-04 16:39:52 +0300

    hasmap lv3

--- src/com/cgi/MyEntry.java
+++ src/com/cgi/MyEntry.java
@@ -10,59 +10,17 @@ public class MyEntry {
 
     public static void main(String[] args) {
 
-        HashMap<Integer, Orc> army = new HashMap<>();
-        int choix = 0;
+        HashMap<Orc, List<Orc>> teams = new HashMap<>();
 
-        while (choix != 4){
-            System.out.println("Que voulez vous faire?");
-            System.out.println("1/ Ajouter un Orc dans mon chateau");
-            System.out.println("2/ Lister les orcs de mon chateau");
-            System.out.println("3/ Afficher un orc par son id");
-            System.out.println("4/ Quitter");
+        teams.put(new Orc("sfdz", 23, 23), Arrays.asList(new Orc("sdgfds", 20, 39), new Orc("gdfg", 20, 39)));
+        teams.put(new Orc("hrzh", 23, 23), Arrays.asList(new Orc("sdgeffds", 20, 39), new Orc("gdaffg", 20, 39)));
 
-            choix = Clavier.lireInt();
-
-            switch (choix){
-                case 1:
-                    System.out.print("ID (unique)>");
-                    int id = Clavier.lireInt();
-
-                    System.out.print("Nom>");
-                    String name = Clavier.lireString();
-
-                    System.out.print("Force>");
-                    int force = Clavier.lireInt();
-
-                    System.out.print("health>");
-                    int health = Clavier.lireInt();
-
-                    if(army.containsKey(id)){
-                        System.out.println("Il existe dejà un Orc avec ce nom.");
-                    }else{
-                        army.put(id, new Orc(name, force, health));
-                        System.out.println("OK");
-                    }
-                    break;
-                case 2:
-                    for (Entry e: army.entrySet()) {
-                        System.out.println(e.getKey() + " --> " + e.getValue());
-                    }
-                    break;
-                case 3:
-                    System.out.print("ID>");
-                    int idSelected = Clavier.lireInt();
-                    if(army.containsKey(idSelected)){
-                        System.out.println(army.get(idSelected));
-                    }else{
-                        System.out.println("Aucun id trouvé...");
-                    }
-                    break;
-                case 4:
-                    break;
-                default:
-                    System.out.println("Mauvaise saisir");
+        int i = 0;
+        for (Entry<Orc, List<Orc>> entry: teams.entrySet()){
+            System.out.println("Chef equipe " + (++i) + ": " +entry.getKey().getName());
+            for (Orc o: entry.getValue()) {
+                System.out.println("\t "+o.getName());
             }
         }
-        System.out.println("Merci");
     }
 }
